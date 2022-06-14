1815a60ba5c7ef536840a649384ae4bad6cc61d2
JMMatosF <jmatosfernandes@live.com.pt>
2022-06-14 23:53:11 +0100

    hey

--- app/src/main/java/com/example/trabalho1/login/LoginActivity.kt
+++ app/src/main/java/com/example/trabalho1/login/LoginActivity.kt
@@ -1,18 +1,25 @@
 package com.example.trabalho1.login
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import com.example.trabalho1.databinding.ActivityLoginBinding
 import com.example.trabalho1.overview.main.OverviewActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     private val viewModel: LoginViewModel by viewModels()
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +29,32 @@ class LoginActivity : AppCompatActivity() {
         initViews()
         initObservers()
 
+    }
+    public override fun onStart() {
+        super.onStart()
+        auth = Firebase.auth
+
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+
+        customToken?.let {
+            auth.signInWithCustomToken(it)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCustomToken:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCustomToken:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+        }
+        Firebase.auth.signOut()
     }
 
     private fun initViews() {
@@ -52,5 +85,6 @@ class LoginActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
 
 }
--- app/src/main/java/com/example/trabalho1/main/MainActivity.kt
+++ app/src/main/java/com/example/trabalho1/main/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.trabalho1.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import com.example.trabalho1.PhotoActivity
 import com.example.trabalho1.databinding.ActivityMainBinding
 import com.example.trabalho1.login.LoginActivity
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +28,16 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity:: class.java)
             startActivity(intent)
         }
+
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
     }
+
 }
